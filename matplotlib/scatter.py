6fda3643bf43e3dd8ddbe949429fee1e99c59bfb
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-16 14:11:55 +0500

    multiple plots for scatter

--- matplotlib/scatter.py
+++ matplotlib/scatter.py
@@ -15,11 +15,27 @@ with open('iris.pickle', 'rb') as f :
 
 sepal_length = iris['data'][:, 0]
 sepal_width = iris['data'][:, 1]
+petal_length = iris['data'][:, 2]
+petal_width = iris['data'][:, 3]
 classes = iris['target']
 
+fig, axes = plt.subplots(2,2)
+axes[0,0].scatter(sepal_length, sepal_width, c=classes)
+axes[0,0].set_xlabel('Sepal length (cm)')
+axes[0,0].set_ylabel('Sepal width (cm)')
 
-plt.scatter(sepal_length, sepal_width, c=classes)
-plt.xlabel('Sepal length (cm)')
-plt.ylabel('Sepal length (cm)')
-plt.title('Iris data: Sepal length v. widthx')
+axes[0,1].scatter(petal_length, petal_width, c=classes)
+axes[0,1].set_xlabel('Petal length (cm)')
+axes[0,1].set_ylabel('Petal width (cm)')
+
+axes[1,0].scatter(sepal_length, petal_length, c=classes)
+axes[1,0].set_xlabel('Sepal length (cm)')
+axes[1,0].set_ylabel('Petal length (cm)')
+
+axes[1,1].scatter(sepal_width, petal_width, c=classes)
+axes[1,1].set_xlabel('Sepal width (cm)')
+axes[1,1].set_ylabel('Petal width (cm)')
+
+
+fig.suptitle('Iris Datase')
 plt.show()
